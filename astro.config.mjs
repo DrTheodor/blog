37549397f4ffe978d03aeb2099e1d81e88a02381
Dr Theo <71725814+DrTheodor@users.noreply.github.com>
2024-03-08 20:16:33 +0300

    right

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { remarkReadingTime } from './src/utils/readTime.ts'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
@@ -30,7 +29,6 @@ export default defineConfig({
 			drafts: true
 		}),
 		sitemap(),
-		tailwind(),
-		pagefind()
+		tailwind()
 	]
 })
